4d49109bfc849be37bffe00ca8ddea1e3e8fe045
nobuyuki-k <nobuyuki.komai@gmail.com>
2022-03-29 05:32:41 +0900

    jsビンゴ:配列をfor文で記述する

--- MyBingo/js/main.js
+++ MyBingo/js/main.js
@@ -1,15 +1,16 @@
 'use strict';
 {
-    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    Math.floor(Math.random() * source.length)
+    // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const source = [];
+    for(let i = 0; i < 15; i++){
+        source[i] = i + 1;
+    }
 
     // splice() メソッドは、その場で既存の要素を取り除いたり、置き換えたり、
     // 新しい要素を追加したりすることで、配列の内容を変更します。
     const b = [];
-    b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    for (let i = 0; i < 5; i++){
+        b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
     console.log(b);
 }
